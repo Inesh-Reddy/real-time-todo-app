a1bc90ec82dd5a5df543997c0d24cb74c1275fe5
Inesh-Reddy <ineshreddy007@gmail.com>
2025-08-07 05:47:19 +0530

    added config for trpc schema generation and cors enabled

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
+import { TRPCModule } from 'nestjs-trpc';
 @Module({
-  imports: [DbModule, TodoModule, ConfigModule.forRoot()],
+  imports: [
+    TRPCModule.forRoot({
+      autoSchemaFile: '../../packages/trpc/src/server',
+    }),
+    DbModule,
+    TodoModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
